7a84b83e6d41ed95fe672ad846e55c5bd1fd8cb6
unknown <emmanuel@chekkitapp.com>
2023-02-20 12:04:58 +0100

    new-design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,45 @@ module.exports = {
     "./src/**/*.{html,js}",
     './pages/**/*.{html,js}',
     './public/**/*.{html,js}',
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
+    screens: {
+      'sm': '480px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '976px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1440px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {
-      container: {
-        center: true,
-        screens: {
-          lg: "1124px",
-          xl: "1124px",
-          "2xl": "1124px",
-        }
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+      animation : {
+        'spin-slow-30': 'spin 30s linear infinite',
+        'spin-slow-25': 'spin 25s linear infinite',
+        'spin-slow-10': 'spin 10s linear infinite',
+        'marquee-infinite' : 'marquee 25s linear infinite',
+      },
+      fontFamily: {
+        'satoshi': ['Satoshi-Regular', 'sans-serif']
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
